9090b77309d5276e9d91ed546d6f2000fc0f0c51
Zy <a451245124@gmail.com>
2020-09-28 10:43:11 +0800

    update index and script
    更新了美食查詢

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var map = L.map('map', {
-    center: [22.604799,120.2976256],
-    zoom: 16
+    center: [25.044955,121.335579],
+    zoom: 10
 });
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -16,7 +16,7 @@ var greenIcon = new L.Icon({
   shadowSize: [41, 41]
 });
 
-//red 美食
+//red food 
 var redIcon = new L.Icon({
   iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
@@ -26,11 +26,23 @@ var redIcon = new L.Icon({
   shadowSize: [41, 41]
 });
 
+// black pharmacy
+var blackIcon = new L.Icon({
+  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+
+
 
 //旅遊
 var markers = new L.MarkerClusterGroup().addTo(map);; //新增一圖層，用來放maker群組
 var xhr = new XMLHttpRequest();
-xhr.open("get","https://raw.githubusercontent.com/listennn08/travelmap/master/static/sightdata.json");
+xhr.open("get","https://raw.githubusercontent.com/a451245124/test/gh-pages/travel.json");
 xhr.send();
 xhr.onload = function(){
  var data = JSON.parse(xhr.responseText).XML_Head.Infos.Info
@@ -46,31 +58,49 @@ xhr.onload = function(){
  map.addLayer(markers);
 }
 
-//美食
+//food
+var markerf = new L.MarkerClusterGroup().addTo(map);;
+var xhr3 = new XMLHttpRequest();
+xhr3.open("get","https://raw.githubusercontent.com/a451245124/test/gh-pages/food.json");
+xhr3.send();
+xhr3.onload = function(){
+ var data = JSON.parse(xhr3.responseText).XML_Head.Infos.Info
+ for(let i =0;data.length>i;i++){
+  
+  markerf.addLayer(L.marker([data[i].Py,data[i].Px], {icon: redIcon}).bindPopup(data[i].Name));
+
+ }
+ map.addLayer(markerf);
+}
+
+/*
+
+//pharmacy 
 var marker2 = new L.MarkerClusterGroup().addTo(map);;
 var xhr2 = new XMLHttpRequest();
-xhr2.open("get","https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json");
+xhr2.open("get","https://raw.githubusercontent.com/a451245124/test/gh-pages/pharmacy.json");
 xhr2.send();
 xhr2.onload = function () {
   var data = JSON.parse(xhr2.responseText).features
   for(let i =0;data.length>i;i++){ 
-    marker2.addLayer(L.marker([data[i].geometry.coordinates[1],data[i].geometry.coordinates[0]], {icon: redIcon}).bindPopup(data[i].properties.name));
+    marker2.addLayer(L.marker([data[i].geometry.coordinates[1],data[i].geometry.coordinates[0]], {icon: blackIcon}).bindPopup(data[i].properties.name));
   }
   map.addLayer(marker2);
 }
 
+*/
 
 
 function aa() {
   var v = document.getElementById("Btnfood").id;
   //history.back() 回上一頁
   map.removeLayer(markers)
-  map.addLayer(marker2)
+  map.addLayer(markerf)
   
 }
 function bb() {
   var v = document.getElementById("Btnfood").id;
   //history.back() 回上一頁
-  map.removeLayer(marker2)
+  map.removeLayer(markerf)
   map.addLayer(markers)
 }
